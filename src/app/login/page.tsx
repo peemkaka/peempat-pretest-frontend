9d5efcc7cc44603814091112f6688919bd12e085
peemkaka <xshot_p@hotmail.com>
2025-06-05 15:29:14 +0700

    [update] button css

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -83,7 +83,7 @@ export default function Login() {
           </div>
 
           <div>
-            <button onClick={() => {}} type="submit">
+            <button className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500" type="submit">
               Login
             </button>
           </div>
